fdf0280ba3077ed807ff773da1e4b2385c66a5cf
Ceasar <ceasarkwadwo@gmail.com>
2022-06-28 12:23:50 +0000

    user request model

--- src/main/java/com/capstone/library/payload/request/UserRequest.java
+++ src/main/java/com/capstone/library/payload/request/UserRequest.java
@@ -5,5 +5,45 @@ import javax.validation.constraints.NotBlank;
 public class UserRequest {
 
     @NotBlank
+    private String username;
 
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String roleType;
+
+    public UserRequest() {
+    }
+
+    public UserRequest(String username, String password, String roleType) {
+        this.username = username;
+        this.password = password;
+        this.roleType = roleType;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(String roleType) {
+        this.roleType = roleType;
+    }
 }
